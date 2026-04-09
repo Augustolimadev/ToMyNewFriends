5bc7a186d42d84f951ba1ab90b44b1d8905c17fb
Augusto Lima <augustolima.dev@gmail.com>
2026-04-09 19:51:57 -0400

    Project creation

---
+++ RedHotChiliDevs/RedHotChiliDevs/RedHotChiliDevsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedHotChiliDevsApp.swift
+//  RedHotChiliDevs
+//
+//  Created by Augusto Lima on 9/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct RedHotChiliDevsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
